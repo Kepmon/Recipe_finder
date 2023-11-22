6e6106b3f7c010c90b69933247395b672da57b27
Monika <monika.kepasa@gmail.com>
2023-11-22 22:38:09 +0100

    added eslint, prettier, and the netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+// eslint-disable-next-line import/no-unresolved
+import svelte from '@astrojs/svelte'
 
-import svelte from "@astrojs/svelte";
+// eslint-disable-next-line import/no-unresolved
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
-});
+  integrations: [svelte()],
+  output: 'server',
+  adapter: netlify()
+})
